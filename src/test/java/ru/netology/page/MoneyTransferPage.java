91c1f30818270f9ab5983821bd1011c146f40697
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-12-05 12:44:19 +0300

    Mistakes corrected

--- src/test/java/ru/netology/page/MoneyTransferPage.java
+++ src/test/java/ru/netology/page/MoneyTransferPage.java
@@ -1,24 +1,35 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import ru.netology.data.DataHelper.*;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
+
 
 public class MoneyTransferPage {
 
-    private static SelenideElement sumField = $("[data-test-id=amount] input");
-    private static SelenideElement cardField = $("[data-test-id='from'] input");
-    private static SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement sumField = $("[data-test-id=amount] input");
+    private SelenideElement cardField = $("[data-test-id='from'] input");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement notEnoughMoneyError = $(withText("Недостаточно средств для совершения перевода!"));
 
-
-    public static void moneyTransfer(CardData cardData, int amount) {
+    public void moneyTransfer(CardData cardData, int amount) {
         sumField.setValue(String.valueOf(amount));
         cardField.setValue(cardData.getCardNumber());
         transferButton.click();
     }
 
+    public void notEnoughMoneyError(CardData cardData, int amount) {
+        sumField.setValue(String.valueOf(amount));
+        cardField.setValue(cardData.getCardNumber());
+        transferButton.click();
+        if (amount > Integer.parseInt(cardData.getCardBalance())) {
+            notEnoughMoneyError.shouldBe(Condition.visible);
+        }
+
+    }
+
 }
-
-
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,70 +1,68 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import ru.netology.data.DataHelper;
 import ru.netology.data.DataHelper.*;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
-import ru.netology.page.MoneyTransferPage;
-import ru.netology.page.VerificationPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selectors.withText;
 
 
 public class MoneyTransferTest {
 
+    DashboardPage dashboardPage;
+
     @BeforeEach
     void setUp() {
-        //Configuration.headless = true;
-        open("http://localhost:9999");
-        LoginPage loginPage = new LoginPage();
-        AuthInfo authInfo = DataHelper.getAuthInfo();
-        VerificationPage verificationPage = loginPage.validLogin(authInfo);
-        VerificationCode verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+        Configuration.headless = true;
+        open("http://localhost:9999", LoginPage.class);
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        dashboardPage = verificationPage.validVerify(verificationCode);
 
     }
 
     @Test
     public void shouldTransferMoneyFromFirstToSecond() {
         int amount = 700;
-        CardData firstCardData = CardData.getFirstCardData();
-        CardData secondCardData = CardData.getSecondCardData();
-        int firstCardBalanceBeforeTransfer = DashboardPage.getFirstCardCurrentBalance();
-        int secondCardCurrentBalanceBeforeTransfer = DashboardPage.getSecondCardCurrentBalance();
-        DashboardPage.secondCard();
-        MoneyTransferPage.moneyTransfer(firstCardData, amount);
+        var firstCardData = CardData.getFirstCardData();
+        var secondCardData = CardData.getSecondCardData();
+        int firstCardBalanceBeforeTransfer = dashboardPage.getFirstCardCurrentBalance();
+        int secondCardCurrentBalanceBeforeTransfer = dashboardPage.getSecondCardCurrentBalance();
+        val moneyTransferPage = dashboardPage.secondCard();
+        moneyTransferPage.moneyTransfer(firstCardData, amount);
         int firstCardBalanceAfterTransfer = DataHelper.balanceCardTransferFrom(firstCardBalanceBeforeTransfer, amount);
         int secondCArdBalanceAfterTransfer = DataHelper.balanceCardTransferTo(secondCardCurrentBalanceBeforeTransfer, amount);
-        int firstCardNewBalance = DashboardPage.getFirstCardCurrentBalance();
-        int secondCArdNewBalance = DashboardPage.getSecondCardCurrentBalance();
+        int firstCardNewBalance = dashboardPage.getFirstCardCurrentBalance();
+        int secondCArdNewBalance = dashboardPage.getSecondCardCurrentBalance();
         assertEquals(firstCardBalanceAfterTransfer, firstCardNewBalance);
         assertEquals(secondCArdBalanceAfterTransfer, secondCArdNewBalance);
-
 
     }
 
     @Test
     public void shouldTransferMoneyFromSecondToFirst() {
         int amount = 400;
-        int firstCardBalanceBeforeTransfer = DashboardPage.getFirstCardCurrentBalance();
-        int secondCardCurrentBalanceBeforeTransfer = DashboardPage.getSecondCardCurrentBalance();
-        DashboardPage.firstCard();
+        var firstCardBalanceBeforeTransfer = dashboardPage.getFirstCardCurrentBalance();
+        var secondCardCurrentBalanceBeforeTransfer = dashboardPage.getSecondCardCurrentBalance();
+        val moneyTransferPage = dashboardPage.firstCard();
         CardData secondCardData = CardData.getSecondCardData();
-        MoneyTransferPage.moneyTransfer(secondCardData, amount);
+        moneyTransferPage.moneyTransfer(secondCardData, amount);
         int firstCardBalanceAfterTransfer = DataHelper.balanceCardTransferTo(firstCardBalanceBeforeTransfer, amount);
         int secondCArdBalanceAfterTransfer = DataHelper.balanceCardTransferFrom(secondCardCurrentBalanceBeforeTransfer, amount);
-        int firstCardNewBalance = DashboardPage.getFirstCardCurrentBalance();
-        int secondCArdNewBalance = DashboardPage.getSecondCardCurrentBalance();
+        int firstCardNewBalance = dashboardPage.getFirstCardCurrentBalance();
+        int secondCArdNewBalance = dashboardPage.getSecondCardCurrentBalance();
         assertEquals(firstCardBalanceAfterTransfer, firstCardNewBalance);
         assertEquals(secondCArdBalanceAfterTransfer, secondCArdNewBalance);
 
@@ -74,14 +72,11 @@ public class MoneyTransferTest {
     @Test
     public void shouldNotTransferIfNotEnoughMoney() {
         int amount = 50_000;
-        CardData firstCardData = CardData.getFirstCardData();
-        CardData secondCardData = CardData.getSecondCardData();
-        int firstCardBalanceBeforeTransfer = DashboardPage.getFirstCardCurrentBalance();
-        int secondCardCurrentBalanceBeforeTransfer = DashboardPage.getSecondCardCurrentBalance();
-        DashboardPage.secondCard();
-        MoneyTransferPage.moneyTransfer(firstCardData, amount);
-        $(withText("Недостаточно средств для совершения перевода!")).shouldBe(Condition.visible);
-
+        var firstCardData = CardData.getFirstCardData();
+        var secondCardData = CardData.getSecondCardData();
+        val moneyTransferPage = dashboardPage.secondCard();
+        moneyTransferPage.moneyTransfer(firstCardData, amount);
+        moneyTransferPage.notEnoughMoneyError(firstCardData, amount);
     }
 
 }
